576981ae5391a21acf728ff6702ec18067fde214
Rafael Correa <tiobenedito@gmail.com>
2022-10-25 18:35:46 +0100

    mockup app form design added and first functions

--- mockup/urls.py
+++ mockup/urls.py
@@ -9,8 +9,10 @@ from django.contrib.auth import views as auth_views
 # from django.conf.urls.static import static
 
 urlpatterns = [
-	# path('', views.index, name='index'),
-    path('', views.mockup, name='mockup'),
+    path('gallery/', views.design_list, name='design_list'),
+    path('design/', views.design, name='design'),
+    path('', views.create, name='create'),
+    path('created_design/', views.created_design, name='created_design'),
     # path('design_form/', views.design_form, name='design_form'),
     # path('designs/new', views.design_create, name='design_create'),
 ]
--- mockup/views.py
+++ mockup/views.py
@@ -16,6 +16,50 @@ from .forms import LoginForm, DesignForm
 from .models import Design
 
 
-def mockup(request):
-    template = 'mockup/mockup.html'
-    return render(request, template)
+def design_list(request):
+    designs = Design.objects.all()
+    return render(request, 'mockup/design_list.html', {'designs': designs})
+
+def design(request):
+    designs = Design.objects.all()
+    return render(request, 'mockup/design.html', {'designs': designs})
+
+# def create(request):
+#     return render(request, 'mockup/create.html')
+
+
+# Design SHOW
+def created_design(request, pk):
+    design = Design.objects.get(id=pk)
+    return render(request, 'mockup/created_design.html', {'design': design})
+
+# Create a new design
+def create(request):
+    designs = Design.objects.all()
+    if request.method == 'POST':
+        form = DesignForm(request.POST, request.FILES)
+        print("LINE 61")
+        if form.is_valid():
+            print("LINE 63")
+            print(request.user)
+            design = form.save(commit=False)
+            design.user = request.user
+            print(design.user)
+            # title = form.cleaned_data['title']
+            # print(title)
+            # design.save()
+            design = form.save()
+            print("LINE 65")
+            return render(request, 'mockup/design_list.html', {'designs': designs})
+    else:
+        form = DesignForm()
+    return render(request, 'mockup/create.html', {'form': form})
+
+# Image File Upload to Database
+# @require_POST
+# def file_upload(request):
+#     save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', request.FILES['file'])
+#     path = default_storage.save(save_path, request.FILES['file'])
+#     document = Document.objects.create(document=path, upload_by=request.user)
+#     return JsonResponse({'document': document.id})
+
