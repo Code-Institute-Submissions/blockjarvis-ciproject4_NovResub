bdeda93e281119e339ac0c5e556f1749fdd6a991
Rafael Correa <tiobenedito@gmail.com>
2022-10-26 11:23:03 +0100

    mocked app refactored

--- mocked/urls.py
+++ mocked/urls.py
@@ -4,22 +4,7 @@ from . import views
 
 from django.contrib.auth import logout
 
-# from django.conf import settings
-# from django.conf.urls.static import static
 
 urlpatterns = [
-    # FOR testing Hello World success
-    # path('', views.hello_world),
-    path('', views.mocked_home, name='mocked_home'),
-	# path('', views.index, name='index'),
-    path('login/', views.login_user, name='login_user'),
-    path('logout_user', logout, name='logout_user'),
-    path('signup/', views.signup_user, name='signup_user'),
-    path('gallery/', views.design_list, name='design_list'),
-    path('design/', views.design, name='design'),
-    path('create/', views.create, name='create'),
-    path('created_design/', views.created_design, name='created_design'),
-    # path('design_form/', views.design_form, name='design_form'),
-    # path('designs/new', views.design_create, name='design_create'),
-
+    path('', views.create, name='create'),
 ]
--- mocked/views.py
+++ mocked/views.py
@@ -17,28 +17,7 @@ from .forms import LoginForm, DesignForm
 from .models import Design
 
 
-def mocked_home(request):
-    return render(request, 'mocked/home.html')
-
-def design_list(request):
-    designs = Design.objects.all()
-    return render(request, 'mocked/design_list.html', {'designs': designs})
-
-def design(request):
-    designs = Design.objects.all()
-    return render(request, 'mocked/design.html', {'designs': designs})
-
-# def create(request):
-#     return render(request, 'mocked/create.html')
-
-
-# Design SHOW
-def created_design(request, pk):
-    design = Design.objects.get(id=pk)
-    return render(request, 'mocked/created_design.html', {'design': design})
-
 # Create a new design
-# @login_required
 def create(request):
     designs = Design.objects.all()
     if request.method == 'POST':
@@ -50,50 +29,9 @@ def create(request):
             design = form.save(commit=False)
             design.user = request.user
             print(design.user)
-            # title = form.cleaned_data['title']
-            # print(title)
-            # design.save()
             design = form.save()
             print("LINE 65")
             return render(request, 'mocked/design_list.html', {'designs': designs})
     else:
         form = DesignForm()
     return render(request, 'mocked/create.html', {'form': form})
-
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        raw_password = request.POST['password']
-        user = authenticate(username=username, password=raw_password)
-        login(request, user)
-        return redirect('mocked_home')
-    else:
-        form = LoginForm()
-    return render(request, 'mocked/login.html', {'form': form})
-
-def logout_user(request):
-    logout(request)
-    return redirect('mocked/mocked_home.html')
-
-def signup_user(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('mocked_home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'mocked/signup.html', {'form': form})
-
-# Image File Upload to Database
-# @require_POST
-# def file_upload(request):
-#     save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', request.FILES['file'])
-#     path = default_storage.save(save_path, request.FILES['file'])
-#     document = Document.objects.create(document=path, upload_by=request.user)
-#     return JsonResponse({'document': document.id})
