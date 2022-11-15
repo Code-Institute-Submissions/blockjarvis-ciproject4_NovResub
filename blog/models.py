9258ba5888e2b81fa2436b5aa002527457745345
Rafael Correa <tiobenedito@gmail.com>
2022-11-15 17:00:37 +0000

    added post method to add order details, text change on blog field

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Blog(models.Model):
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    text = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import Contact
 from .forms import ContactForm
 
@@ -31,7 +32,17 @@ def contact_detail(request, contact_id):
 
 def add_contact(request):
     """ Add an order details  """
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added order details!')
+            return redirect(reverse('add_contact'))
+        else:
+            messages.error(request, 'Failed to add order details. Please ensure the form is valid.')
+    else:    
+        form = ContactForm()
+
     template = 'contact/add_contact.html'
     context = {
         'form': form,
