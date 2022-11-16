8e355b4f4250908b81d46c9b099becd6f2855576
Rafael Correa <tiobenedito@gmail.com>
2022-11-16 10:49:25 +0000

    added email field on submited contact details

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     order = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
